5bc7aa5b0da9ad0b70247b1a73626531e8c532be
AdiIordache <52032857+AdiIordache@users.noreply.github.com>
2022-03-02 20:42:56 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,13 +1,25 @@
 <?php
 
-use Quantox\Database\DataExtractor;
+use Controller\JsonResponseController;
+use Controller\XmlResponseController;
+
+use Database\DataExtractor;
+use Service\StudentService;
 
 require 'vendor/autoload.php';
 
 $dataExtractor = new DataExtractor();
-$dataExtractor->getStudent();
+$service = new StudentService();
 
+
+$jsonResponseController = new JsonResponseController($dataExtractor, $service);
+$xmlResponseController = new XmlResponseController($dataExtractor, $service);
+
+$studentId = $jsonResponseController->getSchoolId();
 $controllerMap = [
-  'getJson' => new ControlerJson(),
-  'getXml' => new ControllerXml()
+    '1' => $jsonResponseController,
+    '2' => $xmlResponseController,
 ];
+
+print (($controllerMap[$studentId])->provideResponse());
+
